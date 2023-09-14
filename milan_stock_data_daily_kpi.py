9a6694ede0dfc8a9376435ff7010649773b239f9
Angelo Mimmo <144590879+angelomimmo@users.noreply.github.com>
2023-09-14 22:27:06 +0200

    Update milan_stock_data_daily_kpi.py

--- milan_stock_data_daily_kpi.py
+++ milan_stock_data_daily_kpi.py
@@ -41,31 +41,23 @@ def lambda_handler(event, context):
     stock_milan_db['date'] = pd.to_datetime(stock_milan_db['date'])
     
     # 2. KPI computing
-    # Ordina il dataframe in base al simbolo e alla data
     stock_milan_db.sort_values(by=['symbol', 'date'], inplace=True)
     
-    # Rimpimento date mancanti
     stock_milan_db = stock_milan_db.groupby('symbol').apply(lambda group: group.fillna(method='ffill'))
     
-    # Crea un elenco di tutte le date uniche presenti nel dataframe
     all_dates = stock_milan_db['date'].unique()
     
-    # Crea una nuova lista di record completi per ogni simbolo
     completed_records = []
     
     for symbol in stock_milan_db['symbol'].unique():
         symbol_data = stock_milan_db[stock_milan_db['symbol'] == symbol]
         
-        # Crea un elenco di tutte le date per questo simbolo
         symbol_dates = symbol_data['date'].unique()
         
-        # Trova la data massima per questo simbolo
         max_date = symbol_dates.max()
         
-        # Crea un elenco di tutte le date mancanti tra la data minima e massima
         missing_dates = pd.date_range(start=symbol_dates.min(), end=max_date).difference(symbol_dates)
         
-        # Crea nuovi record completi per le date mancanti
         for missing_date in missing_dates:
             last_close = symbol_data[symbol_data['date'] < missing_date]['close'].iloc[-1]
             new_record = {
@@ -75,24 +67,19 @@ def lambda_handler(event, context):
             }
             completed_records.append(new_record)
     
-    # Aggiungi i nuovi record completi al dataframe
     completed_df = pd.DataFrame(completed_records)
     
-    # Unisci il dataframe completato con quello originale
     stock_milan_db_completed = pd.concat([stock_milan_db, completed_df], ignore_index=True)
     
     stock_milan_db_completed.sort_values(by=['symbol', 'date'], inplace=True)
     
-    # DB con i risultati
     today = stock_milan_db['date'].max()
     stock_milan_kpi = stock_milan_db_completed[stock_milan_db_completed['date'].isin(
         [today])]
     
-    # Effettua il left join tra stock_milan_kpi e df_symbol_list_italy basato sulla colonna "symbol"
     stock_milan_kpi = stock_milan_kpi.merge(df_symbol_list_italy[['symbol', 'shortName']], on='symbol', how='left')
     stock_milan_kpi.rename(columns={'shortName': 'name'}, inplace=True)
     
-    # Calcola le date di riferimento
     one_day_ago = today - pd.DateOffset(days=1)
     one_week_ago = today - pd.DateOffset(weeks=1)
     one_month_ago = today - pd.DateOffset(months=1)
@@ -101,7 +88,6 @@ def lambda_handler(event, context):
     one_year_ago = today - pd.DateOffset(years=1)
     three_years_ago = today - pd.DateOffset(years=3)
     
-    # Filtra il dataframe solo per le date di interesse
     stock_milan_db_filtered = stock_milan_db_completed[stock_milan_db_completed['symbol'].isin(
         stock_milan_kpi['symbol'])]
     stock_milan_db_filtered = stock_milan_db_filtered[stock_milan_db_filtered['date'].isin(
@@ -110,7 +96,6 @@ def lambda_handler(event, context):
     
     delta_columns = ['1d_delta', '1w_delta', '1m_delta', '3m_delta', '6m_delta', '1y_delta', '3y_delta']
     
-    # Calcola i delta values e aggiungili come colonne a stock_milan_kpi
     for delta_col, delta_date in zip(delta_columns, [one_day_ago, one_week_ago, one_month_ago, three_months_ago,
                                                      six_months_ago, one_year_ago, three_years_ago]):
         delta_values = []
@@ -131,10 +116,8 @@ def lambda_handler(event, context):
     stock_milan_kpi = stock_milan_kpi[['symbol', 'name', 'date', 'close'] + [col for col in stock_milan_kpi.columns if col not in ['symbol', 'name', 'date', 'close']]]
     stock_milan_kpi.sort_values(by=['symbol', 'date'], inplace=True)
     
-    # Calcola i periodi di riferimento
     periods = [one_week_ago, one_month_ago, three_months_ago, six_months_ago, one_year_ago, three_years_ago]
     
-    # Itera attraverso i periodi e calcola i flag
     for period in periods:
         flag_column = f'{period}_flag'
         stock_milan_kpi[flag_column] = None
@@ -164,15 +147,13 @@ def lambda_handler(event, context):
     stock_milan_kpi.rename(columns=flag_column_mapping, inplace=True)
     
     
-    # 3. Scrittura su Google Drive
-    
-    # Carica le credenziali JSON da un file
+    # 3. Writing on Google Drive
+
     google_drive_credentials = os.environ.get('GOOGLE_DRIVE_CREDENTIALS_JSON')
     
     try:
         credentials_dict = json.loads(google_drive_credentials)
         
-        # Crea l'oggetto delle credenziali utilizzando ServiceAccountCredentials
         credentials = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, ['https://www.googleapis.com/auth/drive'])
         
         # Autenticazione con le credenziali
@@ -181,16 +162,16 @@ def lambda_handler(event, context):
         drive = GoogleDrive(gauth)
     
         # Carica il DataFrame su Google Drive
-        data = stock_milan_kpi.to_csv(index=False)  # Converti il DataFrame in CSV
+        data = stock_milan_kpi.to_csv(index=False)
         file = drive.CreateFile({'title': 'stock_milan_kpi.csv',
                                  'id': '1RqrFeQ6w1YF8ZWAcSzCVsg6zbf5PU0u0',
                                  'parents': [{'id': '1Z2X3UyLRhTu0p4NCmedeVnPVzhHJ4F2h'}]})
         file.SetContentString(data)
         file.Upload()
-        print('File caricato su Google Drive con ID:', file.get('id'))
+        print('File uploaded on Google Drive with ID:', file.get('id'))
     
     except json.JSONDecodeError as e:
-        print("Errore nella decodifica JSON:", e)
+        print("Error in decoding JSON:", e)
     
     return {
         'statusCode': 200,
